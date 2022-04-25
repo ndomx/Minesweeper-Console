4e6de85c1c50af6199d69d61e90431c38cf7265c
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2022-04-24 20:21:45 -0400

    Implement GameSetup

--- Program.cs
+++ Program.cs
@@ -6,8 +6,43 @@ namespace minesweeper
     {
         static void Main(string[] args)
         {
-            Board.Initialize(10, 10, 0.5f);
+            GameSetup();
             Board.Print();
+        }
+
+        static void GameSetup()
+        {
+            string input;
+            int width, height;
+
+            Console.Write("Number of rows: ");
+            input = Console.ReadLine();
+            if (!(int.TryParse(input, out height)))
+            {
+                throw new ArgumentException($"Invalid input: {input}");
+            }
+
+            Console.Write("Number of columns: ");
+            input = Console.ReadLine();
+            if (!(int.TryParse(input, out width)))
+            {
+                throw new ArgumentException($"Invalid input: {input}");
+            }
+
+            float bombDensity;
+            Console.Write("Bomb density [0,1]: ");
+            input = Console.ReadLine();
+            if (!(float.TryParse(input, out bombDensity)))
+            {
+                throw new ArgumentException($"Invalid input: {input}");
+            }
+
+            if ((bombDensity < 0) || (bombDensity > 1))
+            {
+                throw new ArgumentOutOfRangeException("Bomb density out of range");
+            }
+
+            Board.Initialize(width, height, bombDensity);
         }
     }
 }
